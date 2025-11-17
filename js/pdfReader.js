5b6334976689f32cc24adcf5a392d80d873d9046
Randy2222222 <randyjpollitt@yahoo.com>
2025-11-17 14:24:06 -0500

    Update pdfReader.js

--- js/pdfReader.js
+++ js/pdfReader.js
@@ -13,11 +13,11 @@
     return;
   }
 
-  // set worker (must set before getDocument in some environments)
-  if (pdfjsLib.GlobalWorkerOptions) {
-    pdfjsLib.GlobalWorkerOptions.workerSrc =
-      "https://cdnjs.cloudflare.com/ajax/libs/pdf.js/3.10.142/pdf.worker.min.js";
-  }
+  // set worker (must set before getDocument in some environments) comment out next 4 lines
+  //if (pdfjsLib.GlobalWorkerOptions) {
+   // pdfjsLib.GlobalWorkerOptions.workerSrc =
+    //  "https://cdnjs.cloudflare.com/ajax/libs/pdf.js/3.10.142/pdf.worker.min.js";
+//  }
 
   // Shared state exported to window so analyzer can access if needed
   window._pdfReader = {
