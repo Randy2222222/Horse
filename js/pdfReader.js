b9d0ffde792a04683d0403db28d0070751fdb5ca
Randy2222222 <randyjpollitt@yahoo.com>
2025-11-20 17:52:59 -0500

    Update pdfReader.js

--- js/pdfReader.js
+++ js/pdfReader.js
@@ -148,7 +148,7 @@ if (window.parsePPTable) {
 
 for (let h of pp) {
  // Replaced line below to change: h.post to h.horsePP
-  // text += "POST POSITION: " + h.horsePP + "\n"; 
+  text += "POST POSITION: " + h.horsePP + "\n"; 
 // text += "POST POSITION: " + h.post + "\n";
   text += "------------------------------------\n";
   text += h.raw + "\n\n";
