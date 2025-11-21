2cbf55146d6918a9467c1e06a248145d96abb3bf
Randy2222222 <randyjpollitt@yahoo.com>
2025-11-21 17:00:06 -0500

    Update pdfReader.js

--- js/pdfReader.js
+++ js/pdfReader.js
@@ -164,20 +164,20 @@ if (window.parsePPTable) {
   // comment out one line before adding a bunch of shit now 4 fucking workflows
   //out.textContent = JSON.stringify(pp, null, 2);
  // commenting out 7 lines
- // let text = "";
-     // for (let h of pp) {
-   //  text += "POST POSITION: " + h.post + "\n";
-     // text += "------------------------------------\n";
-   //   text += h.raw + "\n\n";
-//}
- // out.textContent = text;
+  let text = "";
+    for (let h of pp) {
+      text += "POST POSITION: " + h.post + "\n";
+      text += "------------------------------------\n";
+      text += h.raw + "\n\n";
+  }
+     out.textContent = text;
   // adding another new code🙄
-  const horses = window._pdfReader.horses;
-if (!horses || !horses.length) {
-  out.textContent = "ERROR: Full horse parser returned no results.";
-  return;
-}
-out.textContent = JSON.stringify(horses, null, 2);
+  //const horses = window._pdfReader.horses;
+//if (!horses || !horses.length) {
+ // out.textContent = "ERROR: Full horse parser returned no results.";
+  //return;
+//}
+//out.textContent = JSON.stringify(horses, null, 2);
   // out.textContent = JSON.stringify(horses, null, 2);
   // end of new code 😳
   console.log("CREATE OK — PP Parsed:", pp);
